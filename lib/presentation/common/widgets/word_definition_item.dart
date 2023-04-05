0c02a96ad2350886b1125b0d6cd51b43ae7e96d4
Piotr Łępa <piotrlepadev@gmail.com>
2023-04-05 22:25:03 +0200

    Fix margins

--- lib/presentation/common/widgets/word_definition_item.dart
+++ lib/presentation/common/widgets/word_definition_item.dart
@@ -15,62 +15,75 @@ class WordDefinitionItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
+      children: _getItemsWithSeparators([
+        _getPartOfSpeech(context),
+        _getDefinition(context),
+        _getExamples(context),
+        _getSynonyms(context),
+        _getAntonyms(context),
+      ]),
+    );
+  }
+
+  List<Widget> _getItemsWithSeparators(List<Widget?> items) {
+    const separator = SizedBox(height: 8);
+    return items
+        .whereType<Widget>()
+        .expand((element) => [element, separator])
+        .toList()
+      ..removeLast();
+  }
+
+  Widget? _getPartOfSpeech(BuildContext context) {
+    if (wordDefinition.partOfSpeech == null) return null;
+
+    return SelectableText(
+      wordDefinition.partOfSpeech!,
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 16,
+        color: ThemeProvider.of(context).primaryColor,
+      ),
+    );
+  }
+
+  Widget? _getDefinition(BuildContext context) {
+    return _getSection(
       children: [
-        ..._getPartOfSpeech(context),
-        ..._getDefinition(context),
-        ..._getExamples(context),
-        ..._getSynonyms(context),
-        ..._getAntonyms(context),
+        _getHeader(context.strings.wordDefinition),
+        _getContent(wordDefinition.definition),
       ],
     );
   }
 
-  List<Widget> _getPartOfSpeech(BuildContext context) {
-    if (wordDefinition.partOfSpeech == null) return const [];
-
-    return [
-      SelectableText(
-        wordDefinition.partOfSpeech!,
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 16,
-          color: ThemeProvider.of(context).primaryColor,
-        ),
-      ),
-      _getSeparator(),
-    ];
+  Widget? _getExamples(BuildContext context) {
+    return _getSectionForList(
+      wordDefinition.examples,
+      (items) => [
+        _getHeader(context.strings.wordExamples),
+        ...items.map((example) => _getContent(example)),
+      ],
+    );
   }
 
-  List<Widget> _getDefinition(BuildContext context) {
-    return [
-      _getHeader(context.strings.wordDefinition),
-      _getContent(wordDefinition.definition),
-      _getSeparator(),
-    ];
+  Widget? _getSynonyms(BuildContext context) {
+    return _getSectionForList(
+      wordDefinition.synonyms,
+      (items) => [
+        _getHeader(context.strings.wordSynonyms),
+        _getContent(items.join(', ')),
+      ],
+    );
   }
 
-  List<Widget> _getExamples(BuildContext context) {
-    return wordDefinition.examples.ifNotEmpty((items) => [
-          _getHeader(context.strings.wordExamples),
-          ...items.map((example) => _getContent(example)),
-          _getSeparator(),
-        ]);
-  }
-
-  List<Widget> _getSynonyms(BuildContext context) {
-    return wordDefinition.synonyms.ifNotEmpty((items) => [
-          _getHeader(context.strings.wordSynonyms),
-          _getContent(items.join(', ')),
-          _getSeparator(),
-        ]);
-  }
-
-  List<Widget> _getAntonyms(BuildContext context) {
-    return wordDefinition.antonyms.ifNotEmpty((items) => [
-          _getHeader(context.strings.wordSynonyms),
-          _getContent(items.join(', ')),
-          _getSeparator(),
-        ]);
+  Widget? _getAntonyms(BuildContext context) {
+    return _getSectionForList(
+      wordDefinition.antonyms,
+      (items) => [
+        _getHeader(context.strings.wordSynonyms),
+        _getContent(items.join(', ')),
+      ],
+    );
   }
 
   Widget _getHeader(String text) {
@@ -86,14 +99,22 @@ class WordDefinitionItem extends StatelessWidget {
     return SelectableText(text);
   }
 
-  Widget _getSeparator() {
-    return const SizedBox(height: 8);
+  Widget? _getSectionForList<T>(
+    List<T>? items,
+    List<Widget> Function(List<T> items) widgetBuilder,
+  ) {
+    if ((items ?? []).isEmpty) return null;
+    return _getSection(
+      children: widgetBuilder(items!),
+    );
   }
-}
 
-extension<T> on List<T>? {
-  List<Widget> ifNotEmpty(List<Widget> Function(List<T> items) widgetBuilder) {
-    if ((this ?? []).isEmpty) return [];
-    return widgetBuilder(this!);
+  Widget? _getSection({
+    required List<Widget> children,
+  }) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: children,
+    );
   }
 }
