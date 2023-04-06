7757918ce0e94eea1801ab19621c803b3106560e
Piotr Łępa <piotrlepadev@gmail.com>
2023-04-06 23:04:27 +0200

    Translate successfully even if the definitions not found.

--- lib/domain/use_case/get_info_and_save_text_use_case.dart
+++ lib/domain/use_case/get_info_and_save_text_use_case.dart
@@ -3,8 +3,8 @@ import 'package:english_words/domain/model/saved_text/saved_text.dart';
 import 'package:english_words/domain/model/translations/translations.dart';
 import 'package:english_words/domain/model/word_definitions/word_definitions.dart';
 import 'package:english_words/domain/use_case/get_ipa_transcription_use_case.dart';
-import 'package:english_words/domain/use_case/get_word_definitions_use_case.dart';
 import 'package:english_words/domain/use_case/get_translation_use_case.dart';
+import 'package:english_words/domain/use_case/get_word_definitions_use_case.dart';
 import 'package:english_words/domain/use_case/save_text_use_case.dart';
 import 'package:injectable/injectable.dart';
 
@@ -26,11 +26,11 @@ class GetInfoAndSaveTextUseCase {
     return Future.wait([
       _getTranslationUseCase.invoke(text),
       _getIpaTranscriptionUseCase.invoke(text),
-      _getTextDefinitionsUseCase.invoke(text),
+      _getTextDefinitionsOrNull(text),
     ]).then((data) {
       final translations = data[0] as Translations;
       final ipaTranscription = data[1] as IpaTranscription;
-      final definitions = data[2] as WordDefinitions;
+      final definitions = tryCast<WordDefinitions>(data[2]);
       return SavedText(
         id: null,
         originalText: text,
@@ -44,4 +44,14 @@ class GetInfoAndSaveTextUseCase {
       );
     }).then(_saveTextUseCase.invoke);
   }
+
+  Future<WordDefinitions?> _getTextDefinitionsOrNull(String text) async {
+    try {
+      return await _getTextDefinitionsUseCase.invoke(text);
+    } catch (e) {
+      return Future.value(null);
+    }
+  }
+
+  T? tryCast<T>(x) => x is T ? x : null;
 }
